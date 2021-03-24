ac5c7e5a8d33b8f30d42ce8b43cd0e623f2c5605
utente <utente@LAPTOP-E2PEJK5Q.home-life.hub>
2021-03-24 21:46:11 +0100

    Soluzione punto 2

--- src/main/java/it/polito/tdp/librettovoti/model/Libretto.java
+++ src/main/java/it/polito/tdp/librettovoti/model/Libretto.java
@@ -34,6 +34,44 @@ public class Libretto { //al suo interno contiene tanti coti
 		}								  //scelgo il layout della colonna nella lista
 		return s;
 		}
+	
+	//METODI PER STAMPARE I VOTI UGUALI A UN DATO PUNTEGGIO
+	
+	/*public void stampaVotiUguali(int punteggio) {
+		//il libretto stampa i voti da solo ma è un metodo molto limitante perche sa fare solo quello
+	}
+	
+	public String votiUguali(int punteggio) {
+		//calcola una stringa che contiene i voti e sarà poi il chiamante a stampare (piu generale--> piu utile)
+		//solo il nome o tutto l'esame?
+	}
+	*/ 
+	
+	public List<Voto> listaVotiUguali(int punteggio) {
+		//restituisce i voti uguali al criterio selezionato (con questo stampo l'oggetto intero)
+		//volevi i voti uguali a 25?--> ti do tutti i voti completi uguali a 25 
+		//il DIFETTO di questo metodo è che espone tutta la mia struttura dati interna del voto
+		//e modificassi tutti i miei dati interni poi dovrei modificare tutti i metodi che restituiscono l'oggetto intero
+	
+	List<Voto> risultato = new ArrayList<>(); //nasce come lista vuota
+	for(Voto v: this.voti) {
+		if(v.getVoto()==punteggio)
+			risultato.add(v); //abbiamo aggiunto ad una nuova lista un oggetto gia esistente in un altra (perche non ho new)
+	}
+	
+	return risultato;
+	}
+	
+	
+	public Libretto votiUguali(int punteggio) {
+		//in questo caso restituisco un sottolibretto con i voti richiesti
+		Libretto risultato = new Libretto();
+		for(Voto v: this.voti) {
+			if(v.getVoto()==punteggio)
+				risultato.add(v);
+		}
+		return risultato;
+	}
 }
 
 
--- src/main/java/it/polito/tdp/librettovoti/model/TestLibretto.java
+++ src/main/java/it/polito/tdp/librettovoti/model/TestLibretto.java
@@ -1,6 +1,7 @@
 package it.polito.tdp.librettovoti.model;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public class TestLibretto {
 
@@ -14,11 +15,17 @@ public class TestLibretto {
 		libretto.add(voto1);
 		libretto.add(new Voto("Fisica1", 28, LocalDate.of(2019, 7, 15))); //in questo caso creo il nuovo oggetto qui
 		libretto.add(new Voto("Informatica", 24, LocalDate.of(2019, 9, 15)));
-		
+		libretto.add(new Voto("Chimica", 25, LocalDate.of(2019, 2, 17)));
 		//System.out.println(libretto);--> mi stampa il riferimento all'oggetto libretto se non ho definito i toString
 		//per stampare i contenuti del libretto devo chiedere al libretto come stamparsi 
 		
 		System.out.println(libretto.toString()); //in questo modo posso stampare il contenuto del libretto
 												 //toString potrebbe essere omesso
+		
+		List<Voto> venticinque = libretto.listaVotiUguali(25); //creo una nuova lista dove memorizzo i voti richiesti
+		System.out.println(venticinque);					   //non c è il new perche lo fa il metodo chiamato
+		
+		Libretto venticinquee = libretto.votiUguali(25); //in questo modo ottengo un nuovo libretto
+		System.out.println(venticinquee);	
 	}
 }
